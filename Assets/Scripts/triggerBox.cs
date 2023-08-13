bd88eac19a1c13906899c371c8b2f478a84404fb
Lite2GitHub <ngdiyi2@gmail.com>
2023-08-13 12:39:00 +1000

    somethings

--- Assets/Scripts/triggerBox.cs
+++ Assets/Scripts/triggerBox.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class triggerBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    public GameObject SolvedLight;
+    public GameObject ProblemLight;
+    public BoxCollider Collider;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    bool BoxCollider;
+
 }
